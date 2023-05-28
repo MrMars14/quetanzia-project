7187494c10be19b54ea50674f97b6e7c19795dce
Justin <jtrmars@gmail.com>
2023-05-28 13:25:41 -0400

    beeg change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from './spiral-galaxy.png';
+import mapWorld from './QuetanziaFullMap.jpg'
 import './App.css';
+import React from 'react';
+
+//const mapWorld = require('./public/QuetanziaFullMap.jpg');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className = "App-flex-row">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span>
+            There is much to come...
+          </span>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.google.com/document/d/1m22eWHcS__0zrH0oVFqfVrEGhhPvYOYkQmvEtW22LQU/edit?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          See My Rules
         </a>
       </header>
+      <img src = {mapWorld} className = "App-map"alt = "logo" />
     </div>
   );
 }
